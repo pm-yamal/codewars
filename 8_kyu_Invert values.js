7eaec99734bf13258f87044829713e7ba10f7278
Marat <90244037+pm-yamal@users.noreply.github.com>
2024-04-18 22:58:17 +0500

    Create 8_kyu_Invert values.js

---
+++ 8_kyu_Invert values.js
@@ -0,0 +1,30 @@
+// Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+// invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
+// invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
+// invert([]) == []
+// You can assume that all values are integers. Do not mutate the input array/list.
+
+// 1. Через циклы
+// function invert(array) {
+//   const arrInvert = [];
+//   for (let i = 0; i < array.length; i++) {
+//     if (array[i] !== 0) {
+//       arrInvert.push(-array[i]);
+//     }
+//     else if (array[i] === 0) {
+//       arrInvert.push(0);
+//     }
+//   }
+//   return arrInvert;
+// }
+
+// 2. Через методы
+// const invert = (array) => {
+//   const arrInvert = array.map(i => -i);
+//   return arrInvert;
+// }
+
+// console.log(invert([1,2,3,4,5])) // [-1,-2,-3,-4,-5]
+// console.log(invert([1,-2,3,-4,5])) // [-1,2,-3,4,-5]
+// console.log(invert([])) // []
+// console.log(invert([0])) // [0]
